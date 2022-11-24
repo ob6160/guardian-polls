8f5fc85374fb774b02cabf6855a26620f15f1d60
David Blatcher <dv.bltchr@gmail.com>
2022-11-24 14:15:53 +0000

    fix per

--- components/PollResultsDisplay.tsx
+++ components/PollResultsDisplay.tsx
@@ -3,32 +3,36 @@ import { AnswerAndCount } from "../poll-data/types";
 import ProgressBar from "./ProgressBar";
 
 const testData = [
-  { answer: "test", bgcolor:"#FFE500", completed: 60 },
+  { answer: "test", bgcolor: "#FFE500", completed: 60 },
   { answer: "check", bgcolor: "#F3C100", completed: 40 },
 ];
 
-interface Props {results: AnswerAndCount[]}
+interface Props {
+  results: AnswerAndCount[];
+}
 
-function PollResultsDisplay( props:Props ) {
-    const {results} = props;
-    const total = results.reduce((sum, result) => sum + result.count, 0);
+function PollResultsDisplay(props: Props) {
+  const { results } = props;
+  const total = results.reduce((sum, result) => sum + result.count, 0);
 
+  const [completed, setCompleted] = useState(0);
 
-
-const [completed, setCompleted] = useState(0);
-
-useEffect(() => {
-  setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
-}, []);
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
 
   return (
     <div className="App">
       {results.map((item, idx) => (
-        <ProgressBar key={item.id} answer={item.text} bgcolor={"#FFE500"} completed={item.count*100/total} />
+        <ProgressBar
+          key={item.id}
+          answer={item.text}
+          bgcolor={"#FFE500"}
+          completed={total ? (item.count * 100) / total : 0}
+        />
       ))}
     </div>
   );
 }
 
 export default PollResultsDisplay;
-
